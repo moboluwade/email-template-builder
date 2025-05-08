dc42b634af6cdec4a706861df4952569ead21365
moboluwade <folarinboluwade.com>
2025-05-08 10:50:23 +0100

    feat: make DnD context and drag overlay global

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,25 @@ import "./App.css";
 import Editor from "./components/editor/Editor";
 import Palette from "./components/palette/Palette";
 import ActionPane from "./components/actionPane/ActionPane";
+import { buildingBlocks } from "./lib/buildingBlocks";
 
 import {
   DndContext,
+  DragCancelEvent,
   type DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
   PointerSensor,
   useSensor,
   useSensors,
 } from "@dnd-kit/core";
 
 import { useTemplateStore, type BlockType } from "@/stores/useTemplateStore";
+import { useState } from "react";
 
 function App() {
   const { addBlock } = useTemplateStore();
+  const [activeId, setActiveId] = useState<string | null>(null);
 
   const sensors = useSensors(
     useSensor(PointerSensor, {
@@ -25,6 +31,9 @@ function App() {
     })
   );
 
+  const handleDragStart = (event: DragStartEvent) => {
+    setActiveId(event.active.id as string);
+  };
   const handleDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
 
@@ -37,13 +46,35 @@ function App() {
     }
   };
 
+  const handleDragCancel = () => {
+    setActiveId(null);
+  };
+
+  const activeBlock = activeId
+    ? buildingBlocks.find((block) => block.type === activeId)
+    : null;
+
   return (
-    <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
+    <DndContext
+      sensors={sensors}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragCancel={handleDragCancel}
+    >
       <div className="flex flex-row justify-between h-screen max-w-screen min-w-screen bg-background">
-        <Palette />
+        <Palette buildingBlocks={buildingBlocks} />
         <Editor />
         <ActionPane />
       </div>
+
+      <DragOverlay>
+        {activeId && activeBlock ? (
+          <div className="flex flex-col items-center justify-center p-3 bg-white border border-gray-200 rounded-md cursor-grabbing">
+            <div className="mb-2 text-gray-700">{activeBlock.icon}</div>
+            <span className="text-xs text-gray-600">{activeBlock.label}</span>
+          </div>
+        ) : null}
+      </DragOverlay>
     </DndContext>
   );
 }
